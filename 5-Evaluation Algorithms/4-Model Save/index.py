da2197f515682434130d8a69ee87abec683412e0
Ayman Elkassas <a.qassas@neoxero.com>
2021-09-12 18:00:32 +0200

    done

---
+++ 5-Evaluation Algorithms/4-Model Save/index.py
@@ -0,0 +1,18 @@
+import sklearn.svm as s
+import pickle as pk
+import numpy as np
+
+x = np.random.randint(10,size =20).reshape(4,5)
+y = [5,8,9,6]
+
+model = s.SVR()
+model.fit(x,y)
+
+pk.dump(model , open('saved file2.sav','wb'))
+
+print(model.predict([[2,3,6,5,9]]))
+
+##############################################
+
+savedmodel = pk.load(open('saved file2.sav','rb'))
+print(savedmodel.predict([[2,3,6,5,9]]))
